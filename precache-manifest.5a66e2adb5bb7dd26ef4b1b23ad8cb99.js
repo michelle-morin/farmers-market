0289392f9c308d6d6b67941351ee495a54e5d583
Michelle Morin <morin2m@gmail.com>
2020-04-21 16:25:42 -0700

    Updates

---
+++ precache-manifest.5a66e2adb5bb7dd26ef4b1b23ad8cb99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "697843d4cbca1f60179ebed22d886c55",
+    "url": "/farmers-market/index.html"
+  },
+  {
+    "revision": "f9e7d344adb4d0728964",
+    "url": "/farmers-market/static/css/main.2a21f7fe.chunk.css"
+  },
+  {
+    "revision": "983981343b8053610d14",
+    "url": "/farmers-market/static/js/2.1a57be25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/farmers-market/static/js/2.1a57be25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9e7d344adb4d0728964",
+    "url": "/farmers-market/static/js/main.4d194a3c.chunk.js"
+  },
+  {
+    "revision": "5723a4549f10dd2a7c7d",
+    "url": "/farmers-market/static/js/runtime-main.62031cf1.js"
+  }
+]);
